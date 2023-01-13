e4a6e1aa07579e3fa1d80e27ef3e089fa110df30
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-13 17:32:14 -0300

    projeto django

---
+++ recipes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("HOME")
+
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+
+def sobre(request):
+    return HttpResponse("SOBRE")
