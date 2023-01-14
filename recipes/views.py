fed16c9a4aec50f272dd7b2f5b76d60b3ddf3f80
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-14 13:36:50 -0300

    removendo contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html')
 
 
-def contato(request):
-    return HttpResponse("CONTATO")
 
-
-def sobre(request):
-    return HttpResponse("SOBRE")
