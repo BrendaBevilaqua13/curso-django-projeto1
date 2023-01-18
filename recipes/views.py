2926470aa150c133dd7e37a244028333368c3049
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-18 08:56:37 -0300

    Nova URL para uma nova View

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', {'nome': 'Brenda'})
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', {'nome': 'Brenda'})
