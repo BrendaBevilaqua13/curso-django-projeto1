67dcb68d2cd5a4f6579f837f4191167c56c8b030
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-01-19 21:21:08 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
+from . utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', {'nome': 'Brenda'})
+    return render(request, 'recipes/pages/home.html', {'recipes': [make_recipe() for _ in range(5)]})
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe_view.html', {'nome': 'Brenda'})
+    return render(request, 'recipes/pages/recipe_view.html', {'recipe': make_recipe()})
