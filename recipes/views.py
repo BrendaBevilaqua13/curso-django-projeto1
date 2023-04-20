e388f67452858fbbb7ad7ffd2e77dbd7b2e94dcd
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-04-20 14:45:46 -0300

    alterações na view home e category

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,10 @@ from .models import Recipe
 
 def home(request):
     recipes = Recipe.objects.all().order_by('id')
-
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
+
 def category(request,category_id):
     recipes = Recipe.objects.filter(category__id=category_id).order_by('id')
-
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
 
