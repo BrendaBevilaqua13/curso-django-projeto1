2a48e6da5af0a730d4a0524f3b6ac175db6273fc
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-04-24 17:27:04 -0300

    exibir nome da categoria na url

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . utils.recipes.factory import make_recipe
 from .models import Recipe
+from django.http import Http404
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('id')
@@ -8,7 +9,14 @@ def home(request):
 
 def category(request,category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('id')
-    return render(request, 'recipes/pages/category.html', context={'recipes': recipes})
+    
+    if not recipes:
+        raise Http404('Not Found')
+        
+    return render(request, 'recipes/pages/category.html',
+                  context={'recipes': recipes,
+                           'title': f'{recipes.first().category.name} - Category| '
+                           })
 
 
 def recipe(request, id):
