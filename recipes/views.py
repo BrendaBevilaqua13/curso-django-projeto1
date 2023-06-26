d0d86d6eb73097493d82d6c036d442fa72670701
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-06-26 12:02:53 -0300

    atualizando nome do projeto

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -16,7 +16,7 @@ def category(request,category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by('id').first()    
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     
     return render(request, 'recipes/pages/recipe_view.html',
                   {'recipe': recipe, 'is_detail_page': True})
