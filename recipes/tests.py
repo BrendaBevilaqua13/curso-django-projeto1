1e78ab20f8f96aa64ebc37cdfcf3ef211e1d9e6d
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-06-27 17:02:38 -0300

    criando o package de tests

--- recipes/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        assert 1 == 1
--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,7 @@ def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('id')
     return render(request, 'recipes/pages/home.html', context={'recipes': recipes})
 
+
 def category(request,category_id):        
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('id'))
         
