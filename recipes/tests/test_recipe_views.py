f3e660878ad5caeb0a9b4b5c963a686a8a4b6e93
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-06-29 15:31:40 -0300

    aplicando mais testes no arquivo views

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,14 +4,36 @@ from recipes import views
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_views_function_is_correct(self):
+    def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
         
-    def test_recipe_category_views_function_is_correct(self):
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_recipe_home_view_load_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+        
+    def test_recipe_home_template_shows_not_recipes_found_if_no_recipes(self):
+         response = self.client.get(reverse('recipes:home'))
+         self.assertIn('Nenhuma Receita', response.content.decode('utf-8')) 
+        
+    def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
         
-    def test_recipe_recipe_views_function_is_correct(self):
+    def test_recipe_category_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_recipe_datail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+    
+    def test_recipe_datail_view_returns_404_if_no_recipes_found(self):
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertEqual(response.status_code, 404)
+        
+ 
