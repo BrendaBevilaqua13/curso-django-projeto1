986f96a82003c3653c7e151580fa35cb6fdb1e0e
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-12-13 09:28:11 -0300

    usnado o parameterized no teste

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,11 +1,21 @@
 from .test_recipe_base import RecipeTestBase
+from  django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self):
         self.recipe = self.make_recipe()
         return super().setUp()
+    
+    @parameterized.expand([
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
 
-    def test_the_test(self):
-        recipe = self.recipe
-        ...
