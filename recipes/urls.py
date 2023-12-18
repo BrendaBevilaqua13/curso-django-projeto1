afb00acda3a5bb5e826a0e56c4c526142dd1cd42
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-12-18 11:08:56 -0300

    criando uma nova url para search com TDD

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
     path('recipes/category/<int:category_id>/', views.category, name='category'),
+    path('recipes/search/', lambda request:..., name='search'),
 ]
