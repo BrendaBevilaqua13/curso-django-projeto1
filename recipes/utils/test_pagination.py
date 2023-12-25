638500cc8a206df75c9850d366e0800367d2bb5c
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-12-25 03:32:20 -0300

    iniciando testes de paginação

---
+++ recipes/utils/test_pagination.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+def make_pagination_range(page_range,
+            qtd_paginas,
+            current_page):
+    return [1,2,3,4]
+
+class PaginationTest(TestCase):
+    def test_make_pagination_range_returns_a_pagination_range(self):
+        pagination = make_pagination_range(
+            page_range = list(range(1, 21)),
+            qtd_paginas = 4,
+            current_page = 1
+        )
+        self.assertEqual([1,2,3,4], pagination)
+
+    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
+        
