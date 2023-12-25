45fad6f4ee5f5ea261e4856bf0c6026df425fb4b
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2023-12-25 03:50:05 -0300

    mudando o range de paginas quando necessario

--- recipes/utils/test_pagination.py
+++ recipes/utils/test_pagination.py
@@ -1,18 +1,36 @@
 from unittest import TestCase
+import math
 
 def make_pagination_range(page_range,
-            qtd_paginas,
+            qty_pages,
             current_page):
-    return [1,2,3,4]
+    
+    middle_range = math.ceil(current_page/2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+
+
+    return page_range[start_range:stop_range]
 
 class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range = list(range(1, 21)),
-            qtd_paginas = 4,
+            qty_pages = 4,
             current_page = 1
         )
         self.assertEqual([1,2,3,4], pagination)
 
     def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        
+        pagination = make_pagination_range(
+            page_range = list(range(1, 21)),
+            qty_pages = 4,
+            current_page = 3
+        )
+        self.assertEqual([2,3,4,5], pagination)
