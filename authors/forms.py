8735cfa0454f9781c2de87456f757471d3385889
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-01-09 15:34:39 -0300

    sobreescrevendo os campos do formulario

--- authors/forms.py
+++ authors/forms.py
@@ -9,3 +9,25 @@ class RegisterForm(forms.ModelForm):
                   'username',
                   'email',
                   'password',]
+        
+        labels = {
+            'username': 'Username',
+            'first_name':'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+        error_messages = {
+            'username':{
+                'required':'This field must not be empty',
+            }
+        }
+
+        widgets = {
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
