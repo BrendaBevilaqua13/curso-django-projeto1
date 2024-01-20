e7e0798189b20d555ab6e459decea0bee94efdcc
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-01-20 15:31:10 -0300

    Testando help_text

--- authors/tests/test_uthor_register_form.py
+++ authors/tests/test_uthor_register_form.py
@@ -12,7 +12,15 @@ class AuthorRegisterFormUniTest(TestCase):
        ('password','Type your password'),
        ('password2','Repeat your password'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder_is_correct(self, field, placeholder):
         form = RegisterForm()
         current_placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(placeholder, current_placeholder)
+
+    @parameterized.expand([
+       ('email','The e-mail must be valid'),
+    ])
+    def test_fields_help_text_is_correct(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(needed, current)
