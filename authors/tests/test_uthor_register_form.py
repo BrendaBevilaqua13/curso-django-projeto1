ba132910b8cec18b385ed2fbd88294f5776e5d51
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-01-25 14:55:14 -0300

    Test passwords

--- authors/tests/test_uthor_register_form.py
+++ authors/tests/test_uthor_register_form.py
@@ -84,3 +84,30 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         msg = 'Username must have less than 150 characters.'
         self.assertIn(msg,response.content.decode('utf-8'))
         self.assertIn(msg,response.context['form'].errors.get('username'))
+
+    def test_password_filed_have_lower_upper_case_letters_and_numbers(self):
+        self.form_data['password'] = 'abc123'
+        url = reverse('create')
+        response = self.client.post(url,data=self.form_data, follow=True)
+        msg = ('Password must have at least one uppercase letter, one lowercase letter and one number. The length should be at least 8 characters.')
+        self.assertIn(msg,response.content.decode('utf-8'))
+        self.assertIn(msg,response.context['form'].errors.get('password'))
+
+    def test_password_and_password_confirmation_are_equal(self):
+        self.form_data['password'] = 'abc123'
+        self.form_data['password2'] = '@a123ABC123'
+        url = reverse('create')
+        response = self.client.post(url,data=self.form_data, follow=True)
+
+        msg = 'Password and Password2 must be equal'
+
+        self.assertIn(msg,response.content.decode('utf-8'))
+        self.assertIn(msg,response.context['form'].errors.get('password'))
+
+        self.form_data['password'] = '@A123abc123'
+        self.form_data['password2'] = '@A123abc123'
+
+        url = reverse('create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+
+        self.assertNotIn(msg, response.content.decode('utf-8'))
