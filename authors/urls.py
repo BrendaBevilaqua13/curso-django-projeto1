56ee15aef69e0c29e699c1abd86ad3798e8e6294
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-01-29 20:46:53 -0300

    create login

--- authors/urls.py
+++ authors/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('register/create/', views.register_create, name='create'),
+    path('login/', views.login_view, name='login'),
+    path('login/create/', views.login_create, name='login_create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -29,3 +29,9 @@ def register_create(request):
       del(request.session['register_form_data'])
    return redirect('register')
  
+
+def login_view(request):
+   return render(request, 'authors/pages/login.html')
+
+def login_create(request):
+   return render(request, 'authors/pages/login.html')
