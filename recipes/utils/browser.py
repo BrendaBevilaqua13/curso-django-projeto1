a98b81faa334f9a7e27d61e9ba4429df19397def
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-03 17:10:02 -0300

    Criando a classe base

--- recipes/utils/browser.py
+++ recipes/utils/browser.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
+import os
 
 ROOT_PATH = Path(__file__).parent.parent.parent
 EDGEDRIVER_NAME = 'msedgedriver.exe'
@@ -12,6 +13,9 @@ def make_edge_browser(*options):
     if options is not None:
         for option in options:
             edge_options.add_argument(option)
+    
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        edge_options.add_argument('--headless')
 
     edge_service = Service(executable_path=EDGEDRIVER_PATH)
     browser = webdriver.Edge(service=edge_service,options=edge_options)
