286b55a10a0e5edcbbc578940acb5724b8ff9d29
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-06 13:23:49 -0300

    Criando a base para testes de authors

---
+++ tests/functional_tests/authors/base.py
@@ -0,0 +1,17 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from recipes.utils.browser import make_edge_browser
+import time
+
+
+class AuthorsBaseTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_edge_browser()
+        return super().setUp()
+   
+   
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+    
+    def sleep(self,qdt=10):
+        time.sleep(qdt)
