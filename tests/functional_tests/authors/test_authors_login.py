943702e244a1fe5b0840a3bb36bcd8a45ba814cb
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-11 03:32:50 -0300

    Testando usuário e senha inválidos

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -29,3 +29,45 @@ class AuthorsLoginTest(AuthorsBaseTest):
         #usuario ver a mensagem de sucesso e seu nome
         self.assertIn(f'Your are logged in with {user.username}.',
                       self.browser.find_element(By.TAG_NAME, 'body').text)
+    
+    def test_login_create_raises_404_if_not_post_method(self):
+        self.browser.get(self.live_server_url + '/authors/login/create/')
+
+        self.assertIn('Not Found',
+                      self.browser.find_element(By.TAG_NAME,'body').text)
+
+
+    def test_login_data_invalid_view_message_error(self):
+        str_password = '  '
+        user = User.objects.create_user(username='    ',password=str_password)
+        self.browser.get(self.live_server_url + '/authors/login/')
+
+        #usuario ver o formulario de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys(user.username)
+        password_field.send_keys(str_password)
+
+        form.submit()
+
+        self.assertIn('Invalid username or password',
+                      self.browser.find_element(By.TAG_NAME, 'body').text)
+
+    def test_login_user_authenticated_view_message_error(self):
+        # usuario abre a pagina de login
+        self.browser.get(self.live_server_url + '/authors/login/')
+
+        #usuario ver o formulario de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+        username_field = self.get_by_placeholder(form, 'Type your username')
+        password_field = self.get_by_placeholder(form, 'Type your password')
+
+        username_field.send_keys('brenda')
+        password_field.send_keys('bre@1d')
+
+        form.submit()
+
+        self.assertIn('Invalid credentials',
+                      self.browser.find_element(By.TAG_NAME, 'body').text)
