4700cd78bf1dba6ab2b61ca35792ded365ab2ae5
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-17 13:55:54 -0300

    Salvando as edições da receita na base de dados e request.FILES para cover

--- authors/views.py
+++ authors/views.py
@@ -103,8 +103,22 @@ def dashboard_recipe_edit(request,id):
    
    form = AuthorRecipeForm(
       request.POST or None,
+      files=request.FILES or None,
       instance=recipe
    )
+
+   if form.is_valid():
+      #form valido e posso tentar salvar
+
+      recipe = form.save(commit=False)
+
+      recipe.author=request.user
+      recipe.preparation_steps_is_html = False
+      recipe.is_published = False
+
+      recipe.save()
+      messages.success(request,'Sua receita foi salva com sucesso!')
+      return redirect(reverse('dashboard_recipe_edit',args=(id,)))
    
    return render(request,'authors/pages/dashboard_recipe.html',
                  {
